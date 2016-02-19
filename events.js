e0f7e18843ab01291ec1f96e2ebb8eeadc4ae845
Andrew London <London@Londons-Computer.local>
2016-02-18 22:03:18 -0500

    Initial Commit

---
+++ events.js
@@ -0,0 +1,869 @@
+var events = [
+{
+id: "3652601098",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51883924,
+name: "londomain/map-filter",
+url: "https://api.github.com/repos/londomain/map-filter"
+},
+payload: {
+ref: "master",
+ref_type: "branch",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-17T01:22:12Z"
+},
+{
+id: "3652597021",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51883924,
+name: "londomain/map-filter",
+url: "https://api.github.com/repos/londomain/map-filter"
+},
+payload: {
+ref: null,
+ref_type: "repository",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-17T01:20:43Z"
+},
+{
+id: "3650743110",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51855383,
+name: "londomain/js-functions",
+url: "https://api.github.com/repos/londomain/js-functions"
+},
+payload: {
+ref: "master",
+ref_type: "branch",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-16T17:43:46Z"
+},
+{
+id: "3650736970",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51855383,
+name: "londomain/js-functions",
+url: "https://api.github.com/repos/londomain/js-functions"
+},
+payload: {
+ref: null,
+ref_type: "repository",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-16T17:42:26Z"
+},
+{
+id: "3650731234",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51774637,
+name: "londomain/time-to-read",
+url: "https://api.github.com/repos/londomain/time-to-read"
+},
+payload: {
+ref: "master",
+ref_type: "branch",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-16T17:41:07Z"
+},
+{
+id: "3646280718",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51774637,
+name: "londomain/time-to-read",
+url: "https://api.github.com/repos/londomain/time-to-read"
+},
+payload: {
+ref: null,
+ref_type: "repository",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-15T18:01:47Z"
+},
+{
+id: "3645281163",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51541025,
+name: "londomain/assignmentFunctions",
+url: "https://api.github.com/repos/londomain/assignmentFunctions"
+},
+payload: {
+ref: "master",
+ref_type: "branch",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-15T14:00:00Z"
+},
+{
+id: "3634854200",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51541025,
+name: "londomain/assignmentFunctions",
+url: "https://api.github.com/repos/londomain/assignmentFunctions"
+},
+payload: {
+ref: null,
+ref_type: "repository",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-11T19:33:33Z"
+},
+{
+id: "3634763905",
+type: "PushEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51467716,
+name: "londomain/game",
+url: "https://api.github.com/repos/londomain/game"
+},
+payload: {
+push_id: 973840627,
+size: 1,
+distinct_size: 1,
+ref: "refs/heads/master",
+head: "690c4b3901ff957d6f637ac288ecde8dcf8eb692",
+before: "2fb9546a1ac911ef7c498338835af2a0242a6564",
+commits: [
+{
+sha: "690c4b3901ff957d6f637ac288ecde8dcf8eb692",
+author: {
+email: "London@Londons-Computer.local",
+name: "Andrew London"
+},
+message: "Reformatted game for switch and added object",
+distinct: true,
+url: "https://api.github.com/repos/londomain/game/commits/690c4b3901ff957d6f637ac288ecde8dcf8eb692"
+}
+]
+},
+public: true,
+created_at: "2016-02-11T19:13:45Z"
+},
+{
+id: "3633234186",
+type: "PushEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51467716,
+name: "londomain/game",
+url: "https://api.github.com/repos/londomain/game"
+},
+payload: {
+push_id: 973305393,
+size: 1,
+distinct_size: 1,
+ref: "refs/heads/master",
+head: "2fb9546a1ac911ef7c498338835af2a0242a6564",
+before: "93faa83d8460939c89f7353961d08b150d79935c",
+commits: [
+{
+sha: "2fb9546a1ac911ef7c498338835af2a0242a6564",
+author: {
+email: "London@Londons-Computer.local",
+name: "Andrew London"
+},
+message: "Added functions to start game over",
+distinct: true,
+url: "https://api.github.com/repos/londomain/game/commits/2fb9546a1ac911ef7c498338835af2a0242a6564"
+}
+]
+},
+public: true,
+created_at: "2016-02-11T13:48:28Z"
+},
+{
+id: "3630191137",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51467716,
+name: "londomain/game",
+url: "https://api.github.com/repos/londomain/game"
+},
+payload: {
+ref: "master",
+ref_type: "branch",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-10T19:55:51Z"
+},
+{
+id: "3630186148",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51467716,
+name: "londomain/game",
+url: "https://api.github.com/repos/londomain/game"
+},
+payload: {
+ref: null,
+ref_type: "repository",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-10T19:54:50Z"
+},
+{
+id: "3628432401",
+type: "PushEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51267986,
+name: "londomain/sk8er",
+url: "https://api.github.com/repos/londomain/sk8er"
+},
+payload: {
+push_id: 971604738,
+size: 1,
+distinct_size: 1,
+ref: "refs/heads/master",
+head: "dc6c81925f23ddc971b165aaba9afa01a2b52b0a",
+before: "492ea2f00874fa76ca05a9d2dd11dc03efe02fcc",
+commits: [
+{
+sha: "dc6c81925f23ddc971b165aaba9afa01a2b52b0a",
+author: {
+email: "London@Londons-Computer.local",
+name: "Andrew London"
+},
+message: "Cleanup for responsive and tablet integration",
+distinct: true,
+url: "https://api.github.com/repos/londomain/sk8er/commits/dc6c81925f23ddc971b165aaba9afa01a2b52b0a"
+}
+]
+},
+public: true,
+created_at: "2016-02-10T13:48:07Z"
+},
+{
+id: "3625600135",
+type: "PushEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51267986,
+name: "londomain/sk8er",
+url: "https://api.github.com/repos/londomain/sk8er"
+},
+payload: {
+push_id: 970600784,
+size: 1,
+distinct_size: 1,
+ref: "refs/heads/master",
+head: "492ea2f00874fa76ca05a9d2dd11dc03efe02fcc",
+before: "bf3da0ff8954d51722b203e8fac49727345dcc17",
+commits: [
+{
+sha: "492ea2f00874fa76ca05a9d2dd11dc03efe02fcc",
+author: {
+email: "London@Londons-Computer.local",
+name: "Andrew London"
+},
+message: "Sass Makeover",
+distinct: true,
+url: "https://api.github.com/repos/londomain/sk8er/commits/492ea2f00874fa76ca05a9d2dd11dc03efe02fcc"
+}
+]
+},
+public: true,
+created_at: "2016-02-09T21:12:05Z"
+},
+{
+id: "3623645336",
+type: "PushEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51267986,
+name: "londomain/sk8er",
+url: "https://api.github.com/repos/londomain/sk8er"
+},
+payload: {
+push_id: 969922478,
+size: 1,
+distinct_size: 1,
+ref: "refs/heads/master",
+head: "bf3da0ff8954d51722b203e8fac49727345dcc17",
+before: "e5aaeb1b30457d1bc161a8d6fe559d133bc2fefd",
+commits: [
+{
+sha: "bf3da0ff8954d51722b203e8fac49727345dcc17",
+author: {
+email: "London@Londons-Computer.local",
+name: "Andrew London"
+},
+message: "Responsive Design for IPHONE",
+distinct: true,
+url: "https://api.github.com/repos/londomain/sk8er/commits/bf3da0ff8954d51722b203e8fac49727345dcc17"
+}
+]
+},
+public: true,
+created_at: "2016-02-09T14:04:39Z"
+},
+{
+id: "3618915503",
+type: "PushEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51267986,
+name: "londomain/sk8er",
+url: "https://api.github.com/repos/londomain/sk8er"
+},
+payload: {
+push_id: 968257093,
+size: 1,
+distinct_size: 1,
+ref: "refs/heads/master",
+head: "e5aaeb1b30457d1bc161a8d6fe559d133bc2fefd",
+before: "6c4da13cb582014c5ac5ab826c15247233d1af47",
+commits: [
+{
+sha: "e5aaeb1b30457d1bc161a8d6fe559d133bc2fefd",
+author: {
+email: "London@Londons-Computer.local",
+name: "Andrew London"
+},
+message: "Added Social icons",
+distinct: true,
+url: "https://api.github.com/repos/londomain/sk8er/commits/e5aaeb1b30457d1bc161a8d6fe559d133bc2fefd"
+}
+]
+},
+public: true,
+created_at: "2016-02-08T14:02:37Z"
+},
+{
+id: "3617013545",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51267986,
+name: "londomain/sk8er",
+url: "https://api.github.com/repos/londomain/sk8er"
+},
+payload: {
+ref: "master",
+ref_type: "branch",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-07T21:50:50Z"
+},
+{
+id: "3617011686",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51267986,
+name: "londomain/sk8er",
+url: "https://api.github.com/repos/londomain/sk8er"
+},
+payload: {
+ref: null,
+ref_type: "repository",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-07T21:49:24Z"
+},
+{
+id: "3607029206",
+type: "PushEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51017290,
+name: "londomain/surf-paddle",
+url: "https://api.github.com/repos/londomain/surf-paddle"
+},
+payload: {
+push_id: 963834625,
+size: 1,
+distinct_size: 1,
+ref: "refs/heads/master",
+head: "22110b2d5e30d785065555e793c0907cb2aa5e6e",
+before: "687dceac120f4833021ac8189475e2bfc5e43568",
+commits: [
+{
+sha: "22110b2d5e30d785065555e793c0907cb2aa5e6e",
+author: {
+email: "London@Londons-Computer.local",
+name: "Andrew London"
+},
+message: "Failed attempt to expand on wireframe",
+distinct: true,
+url: "https://api.github.com/repos/londomain/surf-paddle/commits/22110b2d5e30d785065555e793c0907cb2aa5e6e"
+}
+]
+},
+public: true,
+created_at: "2016-02-04T14:05:51Z"
+},
+{
+id: "3603416476",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51017290,
+name: "londomain/surf-paddle",
+url: "https://api.github.com/repos/londomain/surf-paddle"
+},
+payload: {
+ref: "master",
+ref_type: "branch",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-03T18:58:52Z"
+},
+{
+id: "3603010077",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51017290,
+name: "londomain/surf-paddle",
+url: "https://api.github.com/repos/londomain/surf-paddle"
+},
+payload: {
+ref: null,
+ref_type: "repository",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-03T17:29:25Z"
+},
+{
+id: "3602370529",
+type: "PushEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51005432,
+name: "londomain/test-repo",
+url: "https://api.github.com/repos/londomain/test-repo"
+},
+payload: {
+push_id: 962170250,
+size: 1,
+distinct_size: 1,
+ref: "refs/heads/master",
+head: "184677208ba4a423acdc8dc281b399d3da060496",
+before: "9e600768b99f60621cc140469bcaf57a1c8f9afa",
+commits: [
+{
+sha: "184677208ba4a423acdc8dc281b399d3da060496",
+author: {
+email: "London@Londons-Computer.local",
+name: "Andrew London"
+},
+message: "Set up framework",
+distinct: true,
+url: "https://api.github.com/repos/londomain/test-repo/commits/184677208ba4a423acdc8dc281b399d3da060496"
+}
+]
+},
+public: true,
+created_at: "2016-02-03T15:26:40Z"
+},
+{
+id: "3602138515",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51005432,
+name: "londomain/test-repo",
+url: "https://api.github.com/repos/londomain/test-repo"
+},
+payload: {
+ref: "master",
+ref_type: "branch",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-03T14:37:25Z"
+},
+{
+id: "3602135567",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 51005432,
+name: "londomain/test-repo",
+url: "https://api.github.com/repos/londomain/test-repo"
+},
+payload: {
+ref: null,
+ref_type: "repository",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-03T14:36:45Z"
+},
+{
+id: "3599144565",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 50954899,
+name: "londomain/html-layouts-octocats",
+url: "https://api.github.com/repos/londomain/html-layouts-octocats"
+},
+payload: {
+ref: "master",
+ref_type: "branch",
+master_branch: "master",
+description: "Octocats",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-02T22:02:35Z"
+},
+{
+id: "3599127082",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 50954899,
+name: "londomain/html-layouts-octocats",
+url: "https://api.github.com/repos/londomain/html-layouts-octocats"
+},
+payload: {
+ref: null,
+ref_type: "repository",
+master_branch: "master",
+description: "Octocats",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-02T21:58:34Z"
+},
+{
+id: "3598116487",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 50940894,
+name: "londomain/Lazy-Bean-Site",
+url: "https://api.github.com/repos/londomain/Lazy-Bean-Site"
+},
+payload: {
+ref: "master",
+ref_type: "branch",
+master_branch: "master",
+description: "website for coffee business",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-02T18:13:14Z"
+},
+{
+id: "3598105907",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 50940894,
+name: "londomain/Lazy-Bean-Site",
+url: "https://api.github.com/repos/londomain/Lazy-Bean-Site"
+},
+payload: {
+ref: null,
+ref_type: "repository",
+master_branch: "master",
+description: "website for coffee business",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-02T18:10:57Z"
+},
+{
+id: "3588179572",
+type: "CreateEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 50742915,
+name: "londomain/tubular",
+url: "https://api.github.com/repos/londomain/tubular"
+},
+payload: {
+ref: "working-branch",
+ref_type: "branch",
+master_branch: "master",
+description: "jQuery tubular turns cat videos into website backgrounds. It's that dangerous.",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-01-30T21:00:10Z"
+},
+{
+id: "3588170662",
+type: "PushEvent",
+actor: {
+id: 9470862,
+login: "londomain",
+gravatar_id: "",
+url: "https://api.github.com/users/londomain",
+avatar_url: "https://avatars.githubusercontent.com/u/9470862?"
+},
+repo: {
+id: 50742915,
+name: "londomain/tubular",
+url: "https://api.github.com/repos/londomain/tubular"
+},
+payload: {
+push_id: 956981266,
+size: 1,
+distinct_size: 1,
+ref: "refs/heads/master",
+head: "13c42771dd12f6faeb6e0956d2a4e674c125fe8b",
+before: "6b2ba855560d85cbddecfba2ebf47ce5a1ab6c01",
+commits: [
+{
+sha: "13c42771dd12f6faeb6e0956d2a4e674c125fe8b",
+author: {
+email: "London@Londons-Computer.local",
+name: "Andrew London"
+},
+message: "Reverted text changes made",
+distinct: true,
+url: "https://api.github.com/repos/londomain/tubular/commits/13c42771dd12f6faeb6e0956d2a4e674c125fe8b"
+}
+]
+},
+public: true,
+created_at: "2016-01-30T20:53:04Z"
+}
+]
---
+++ main.js
@@ -0,0 +1,80 @@
+/* jshint ignore:start */
+
+$('.profilePic').append('<img src="' + user.avatar_url + '">');
+$('#name').text(user.name);
+$('#login').append(user.login);
+$('.location').append(user.location);
+
+
+
+$(".tabs").on("click", function(event) {
+  event.preventDefault();
+  $(".tabs").removeClass("active");
+  $(this).addClass("active");
+  var selector = "." + $(this).attr("rel");
+  $(selector).siblings().removeClass("show");
+  $(selector).addClass("show");
+});
+
+
+var repoEach = "";
+_.each(repos, function(item) {
+  repoEach += "<li class= 'repoItems'>"
+  + "<a href = " + item.html_url + ">" + item.name + "</a>"
+  + "<div class = 'timeUpdated'>"
+  + moment.utc(item.updated_at).fromNow()
+  + "</div>"
+  + "</li>";
+
+});
+
+$(".repoList").append(repoEach);
+
+
+var newHtml = ""
+_.each (events, function (item) {
+  if (item.type === "CreateEvent" && item.ref_type ==="branch" ) {
+  newHtml += "<li class ='pubItems'>"
+  + "<span class='octicon octicon-git-branch dashboard-event-icon'></span>"
+  + item.actor.login
+  + " created branch "
+  + item.payload.ref
+  + " at "
+  + item.repo.name
+  + " "
+  + moment.utc(item.created_at).fromNow()
+  + "</li>"
+}
+  else if (item.type === "CreateEvent" && item.ref_type === "repository") {
+  newHtml += "<li class ='pubItems'>"
+  + "<span class='octicon octicon-repo dashboard-event-icon'></span>"
+  + item.actor.login
+  + " created repository "
+  + item.repo.name
+  + " "
+  + moment.utc(item.created_at).fromNow()
+  + "</li>"
+  }
+
+  else if (item.type === "PushEvent") {
+    newHtml += "<li class ='pubItems'>"
+    + "<span class='octicon octicon-git-commit dashboard-event-icon'></span>"
+    + "<div class='time'>" + moment.utc(item.created_at).fromNow() + "</div>"
+    + "<div class='pushLine'>"
+    + item.actor.login
+    + " pushed to "
+    + item.payload.ref
+    + " at "
+    + item.repo.name
+    + "</div>"
+    + "<div class= 'description'>"
+    + item.avatar_url
+    + "<span class='octicon octicon-mark-github'></span>"
+    + item.payload.head.slice(-9)
+    + item.message
+    + "</li>"
+  }
+
+})
+console.log(newHTML)
+// $('.public-activity-sec').append(newHTML)
